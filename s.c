3d6972ee1569512dc3c70a25f28ce6e53037838f
sangaviiii <sangavi.mohana@gmail.com>
2016-06-07 11:57:39 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	printf("Gvien number is zero");
+	else if(n>0)
+	printf("Given number is positive");
+	else if(n<0)
+	printf("Given number is negative");
+	return 0;
+}
